af31a2242cf3d6ec7ba67fd0a7e31f8c6932449a
Alcoda <36179726+Alcoda@users.noreply.github.com>
2019-01-14 10:03:26 -0700

    Login Window

---
+++ src/Dental_Office/LoginWindow.java
@@ -0,0 +1,77 @@
+package Dental_Office;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class LoginWindow extends JFrame implements ActionListener{
+    
+    private JPanel mainPanel;
+    private JLabel loginLbl;
+    private JLabel userLbl;
+    private JLabel passLbl;
+    private JTextField userField;
+    private JTextField passField;
+    private JButton loginBtn;
+    
+    public LoginWindow() {
+        
+        super("Dental Office");
+        setSize(350, 300);
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        
+        mainPanel = new JPanel();
+        mainPanel.setBounds(0, 0, 350, 300);
+        mainPanel.setLayout(null);
+        add(mainPanel);
+        
+        loginLbl = new JLabel("Login");
+        loginLbl.setFont(new Font("Arial", Font.PLAIN, 30));
+        loginLbl.setBounds(15, 10, 80, 50);
+        mainPanel.add(loginLbl);
+        
+        userLbl = new JLabel("Username:");
+        userLbl.setBounds(15, 100, 80, 20);
+        mainPanel.add(userLbl);
+        
+        userField = new JTextField();
+        userField.setBounds(100, 100, 150, 20);
+        mainPanel.add(userField);
+        
+        passLbl = new JLabel("Password:");
+        passLbl.setBounds(15, 150, 80, 20);
+        mainPanel.add(passLbl);
+        
+        passField = new JTextField();
+        passField.setBounds(100, 150, 150, 20);
+        mainPanel.add(passField);
+        
+        loginBtn = new JButton("Login");
+        loginBtn.setBounds(15, 200, 80, 30);
+        mainPanel.add(loginBtn);
+        
+        loginBtn.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+        System.out.println("E");
+        
+        if(userField.getText().equalsIgnoreCase("sec")) {
+            
+            if(passField.getText().equals("ret")) {
+                
+                new MakeAppointment().setVisible(true);
+            }
+        }
+    }
+}
---
+++ src/Dental_Office/Main.java
@@ -0,0 +1,26 @@
+package Dental_Office;
+
+import java.util.ArrayList;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    
+    public static ArrayList<String> usernames = new ArrayList();
+    public static ArrayList<String> passwords = new ArrayList();
+    
+    public static void main(String[] args) {
+        
+        usernames.add("sec");
+        passwords.add("ret");
+        usernames.add("dent");
+        passwords.add("ist");
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginWindow();
+            }
+        });
+    }
+    
+}
